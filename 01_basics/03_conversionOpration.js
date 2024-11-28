946bc7041ae1403c9df4a7a6b04a0f766c0dd53b
Rohit95-gif <rsdev1221@gmail.com>
2024-11-28 16:22:23 +0530

    Update 03_conversionOpration.js

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -30,16 +30,64 @@ let stringName = String(studName)
 
 */
 
-let floatNumber = 1234.54
 
-let floatNum =(floatNumber)
-console.log(floatNum);
-console.log(typeof floatNum);
+//  /console.log(2+2);
 
-/*
-this output will be
+// console.log(2-2);
 
-    float => float
-    number = > float
-    string => 
-*/
+// console.log(2*2);
+
+// console.log(2/2);
+
+// console.log(2%2);
+
+//this is aalll is normal opration 
+
+
+//console.log(2+2+"3");
+ //the output will be 43 because first addition opration is done than sring will join the number 
+
+ //console.log("3"+3+5);
+ //the output will be 335 because you can wite first sting so that string join the addition number and make a stirng 
+
+ //console.log(2*8/16-2);
+ // this is not a good way to write the code is t is not readable and it not recommende by the company
+
+ //better way to do that some thing 
+ //console.log((2*8)/(16)-2);
+
+ let Num1 , Num2 , Num3
+
+ Num1 , Num2 , Num3 =2+2
+    //it is very nice to seen but it not good way to wire a code it's not a good prectice 
+
+    //premetrix and postmetrix
+
+    let number = 100
+
+    number++
+    console.log(number);
+//this output will be 101 it is postmetrix incriment 
+
+ let number2 = 101
+console.log(number2)
+//this output will be 101
+++number2
+console.log(number2)
+
+//this out put will be 102 is called premetrix increments
+
+
+
+    
+    
+
+
+ 
+ 
+
+ 
+
+
+
+
