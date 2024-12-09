04f51bad4c7bacd9bc4c610d7774b29d29f7c7de
Rohit95-gif <rsdev1221@gmail.com>
2024-12-09 21:32:34 +0530

    Create 02_scope.js

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,24 @@
+//this is call the globle scope you cna define variable in globle sope you are able to access the variable in any place 
+//example
+const variableAb = "rohit"
+//you can able to access the variableAb in any placed in any time 
+
+//this type of scope call the block level score
+ if(true){
+    let letDeclare = 45
+   const contDeclare = 78
+    var verDeclare = 56
+    //console.log(letDeclare);
+    // the block level scope u can call the variable under the block u can not call variable outside of the block 
+    // letDeclare is not defined 
+    //the node given this type of error
+    
+ }
+
+ console.log(verDeclare);
+ //this resone most of the morden coder is not use var as a variable declaration it's not define is scope
+ //that's the resone avoid var to declare variable 
+
+
+ console.log(variableAb);
+ 
